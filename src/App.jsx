6f1c34be3ba5ba4ffea8e48665a2ed0f25647f31
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-08-15 19:10:16 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+//import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Chat from "./components/Chat";
+//import SideNav from "./components/SideNav";
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/messages" element={<Chat />} />
+
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import { StrictMode } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+//import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
     <App />
-  </StrictMode>,
-)
+  </Router>  
+  )
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Router>
+//     <App />
+//   </Router>  
+//   )
+
+//   createRoot(document.getElementById('root')).render(
+//     <StrictMode>
+//       <App />
+//     </StrictMode>,
+//   )
+  
+  //  <React.StrictMode> 
+  //   <App /> 
+  //  </React.StrictMode> 
