c140ca0fbfee203f3a1b2fdf1c2d01ee180b570d
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-08-16 14:08:52 +0200

    .

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import css from 'rollup-plugin-css-only'; // Ensure you install this plugin
+ import css from "rollup-plugin-css-only";
 
 export default {
   input: 'src/main.js', // Replace with your actual entry file
   output: {
     file: 'dist/bundle.js', // Output file for your JavaScript
-    format: 'iife', // 'iife', 'cjs', 'es', or 'umd' depending on your needs
+    format: 'iife', // 'iife', 'cjs', 'es', or 'umd'
   },
   plugins: [
     css({ output: 'dist/styles.css' }) // Output CSS to a file
