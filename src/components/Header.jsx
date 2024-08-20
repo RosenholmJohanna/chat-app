e4b89b0c376185aac18905a96feb075beb9141ce
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-08-20 15:10:10 +0200

    added links with navigation in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { selectUser } from "../authSlice";
 import Logout from "./Logout";
@@ -9,16 +10,20 @@ const Header = () => {
 
   return (
     <HeaderStyled>
-      <HeaderText>Chatify</HeaderText>
-      <Logout />
-
       <Img>
         <img
           src={user.avatar}
           alt="User Avatar"
           style={{ width: "70px", height: "70px", borderRadius: "100%" }}
         />
+         <p>{user.user}</p>
       </Img>
+
+      <Link to="/profile"><A>PROFILE</A></Link>
+      <Link to="/messages"><A>CHAT</A></Link>
+      <Link to="/login"><A>LOGIN</A></Link>
+      <Logout />
+     
        {/* <button onClick={() => methodDoesNotExist()}>Break the world</button>;  */}
     </HeaderStyled>
   );
@@ -39,4 +44,21 @@ export const HeaderText = styled.h3`
 
 export const Img = styled.div`
   justify-content: flex-end;
+  
+  p{
+    font-size: 0%.8;
+  color: #c27b7b;
+  }
 `;
+
+export const A = styled.a`
+  justify-content: flex-end;
+  text-decoration: none;
+  font-size: 1em;
+  color: #c27b7b;
+  display: block;
+  transition: 0.3s;
+  font-weight: 400;
+`;
+
+
